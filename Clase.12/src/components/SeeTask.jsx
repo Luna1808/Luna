3802566b9867ef08d16bc36616e65e05f53ab110
Luna1808 <160421842+Luna1808@users.noreply.github.com>
2024-09-14 07:42:50 -0500

    Actividad 4

---
+++ Clase.12/src/components/SeeTask.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+const SeeTask = ({ listTask }) => {
+  const elements = listTask.map((item, index) => (
+    <li key={index}>
+      <h2>{item.title || "Sin título"}</h2>
+      <p>{item.description || "Sin descripción"}</p>
+    </li>
+  ));
+
+  return (
+    <div>
+      <h1>Lista de tareas</h1>
+      <div className="task-container">
+        <ul>{elements}</ul>
+      </div>{" "}
+    </div>
+  );
+};
+
+SeeTask.propTypes = {
+  listTask: PropTypes.array.isRequired,
+};
+
+export default SeeTask;
